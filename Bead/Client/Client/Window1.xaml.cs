d0fcfee9a25ce7eac7557ecd2be2a843f44e8ade
bsanyi99 <bs271999@mailbox.unideb.hu>
2021-11-10 09:30:25 +0100

    Squashed commit of the following:
    commit b6d99afa76ac2783d967d684b739441dfeee1399
    Author: bsanyi99 <bs271999@mailbox.unideb.hu>
    Date:   Wed Nov 10 09:24:52 2021 +0100
    
        Date_simulation
    
        Szimuláció a 2020-as évhez, percekre pontva.
    
    commit 3fc82ecbaf0919c78422871691b8029c15b42dc5
    Merge: 72cb4a8 81aff85
    Author: bsanyi99 <bs271999@mailbox.unideb.hu>
    Date:   Wed Nov 3 23:16:04 2021 +0100
    
        Merge branch 'Gabi' into Sanyi
    
    commit 81aff858f7eccd51846712352b85f61d36bb4377
    Author: kriga1 <71764591+kriczky@users.noreply.github.com>
    Date:   Tue Nov 2 16:30:28 2021 +0100
    
        Óra
    
        Óra feltöltés
    
    commit 72cb4a8443f8af1e38bbc0f3c62f7ffe0d5d0e30
    Merge: cab1fc0 e75981c
    Author: bsanyi99 <bs271999@mailbox.unideb.hu>
    Date:   Mon Oct 25 20:52:42 2021 +0200
    
        Merge branch 'Gabi' into Sanyi
    
    commit e75981cbde3a67be8c49094d19f368cabe8c77a7
    Author: kriga1 <71764591+kriczky@users.noreply.github.com>
    Date:   Mon Oct 25 20:34:03 2021 +0200
    
        Kinti_ho
    
    commit cab1fc0b84237575a31f5b6f0a539224280609c3
    Author: bsanyi99 <bs271999@mailbox.unideb.hu>
    Date:   Wed Oct 20 00:38:18 2021 +0200
    
        Delete 123.txt
    
    commit 4ab7bec815000e5f76e16054659383114203410d
    Author: bsanyi99 <bs271999@mailbox.unideb.hu>
    Date:   Wed Oct 20 00:36:17 2021 +0200
    
        button
    
        Heat and cool button improve
    
    commit bba48e02dd068d070da13e83d6b09334690e0aa9
    Author: bsanyi99 <bs271999@mailbox.unideb.hu>
    Date:   Tue Oct 19 12:38:15 2021 +0200
    
        proba
    
    commit bbfb40de0302605e6e8f016bb7160536fc5c14c6
    Author: kriga1 <71764591+kriczky@users.noreply.github.com>
    Date:   Tue Oct 12 14:02:49 2021 +0200
    
        szenzor
    
    commit 412f73373d6336a8c4f9ddc4846d63e1b98ba313
    Author: kriga1 <71764591+kriczky@users.noreply.github.com>
    Date:   Fri Oct 8 12:57:03 2021 +0200
    
        Delete ads.txt

---
+++ Bead/Client/Client/Window1.xaml.cs
@@ -0,0 +1,502 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public Socket clientSocket;
+        public string userName;
+        public string[] users;
+        byte[] byteData = new byte[2048];
+        bool isNappal = false;
+        bool isEste = false;
+
+        private delegate void UpdateMessageDelegate(string pMessage);
+        private delegate void UpdateOnlineUsersDelegate(string pMessage);
+        private delegate void SendLogoutMessageDelegate();
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        public Window1(Socket clientSocket, String userName)
+        {
+            InitializeComponent();
+            this.clientSocket = clientSocket;
+            this.userName = userName;
+
+
+            clientSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None, new AsyncCallback(OnRecive), clientSocket);
+        }
+
+        private void OnRecive(IAsyncResult ar)
+        {
+
+            byte[] commandType = new byte[4];
+
+            Buffer.BlockCopy(byteData, 0, commandType, 0, 4);
+            //BitConverter.ToInt32(commandType, 0);
+
+            Data dataReceived = new Data(byteData);
+
+
+            if (dataReceived.cmdCommand == Command.Message)
+            {
+
+                string msg = dataReceived.strName + " Üzeni " + dataReceived.strMessage + "\n";
+
+            }
+            else if (dataReceived.cmdCommand == Command.List)
+            {
+                string msg = dataReceived.strMessage;
+
+            }
+
+            clientSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None, new AsyncCallback(OnRecive), clientSocket);
+
+
+        }
+
+
+
+        private void Kitchen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Living_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BadRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Heat_Click(object sender, RoutedEventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(Heating));
+            t.Start();
+            Data dataToSend = new Data();
+            dataToSend.cmdCommand = Command.Heat;
+
+            byte[] data = dataToSend.toByte();
+            clientSocket.Send(data);
+
+        }
+
+        private void Heating()
+        {
+            int temp = 25;
+            for (int i = 0; i < 46; i++)
+            {
+                Console.WriteLine($"{i}.perc utan a viz homerseklete: {temp}°C");
+                Thread.Sleep(1000);
+                temp += 1;
+            }
+
+        }
+
+        private void Cool_Click(object sender, RoutedEventArgs e)
+        {
+            Data dataToSend = new Data();
+            dataToSend.cmdCommand = Command.Cool;
+
+            byte[] data = dataToSend.toByte();
+            clientSocket.Send(data);
+
+        }
+
+
+        private void Logout(object sender, RoutedEventArgs e)
+        {
+            Data msgToSend = new Data();
+            msgToSend.cmdCommand = Command.Logout;
+            msgToSend.strName = userName;
+
+            byte[] message = new byte[2200000];
+            message = msgToSend.toByte();
+            clientSocket.Send(message);
+            Close();
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Fullscreen(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            Thread t = new Thread(new ThreadStart(Work));
+            t.Start();
+
+
+            ////// Ez az lámpa inditó
+            Thread t2 = new Thread(new ThreadStart(DateClock));
+            t2.Start();
+
+
+        }
+
+
+
+        private void Work()
+        {
+            int kinti = 1;
+            int label_ertek = 0;
+            int allando = 20;
+            bool isFutes = false;
+
+            this.Dispatcher.Invoke((Action)(() =>
+            {//this refer to form in WPF application 
+                string s = this.kint_ho.Text;
+                string s1 = this.label1.Content.ToString();
+                kinti = int.Parse(s);
+                label_ertek = int.Parse(s1);
+
+
+            }));
+            if (kinti != allando)
+            {
+                if (kinti < allando)
+                {
+                    /**Fűtés**/
+
+                    int i = label_ertek;
+                    while (i >= label_ertek - 4)
+                    {
+                        Updater uiUpdater = new Updater(UpdateUI);
+                        Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, i);
+                        Thread.Sleep(300);
+                        i--;
+                    }
+
+                    this.Dispatcher.Invoke(delegate ()
+                    {
+                        label1.Background = Brushes.Red;
+                    });
+
+                    for (int j = i; j <= allando; j++)
+                    {
+
+                        Updater uiUpdater = new Updater(UpdateUI);
+                        Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, j);
+                        Thread.Sleep(300);
+                    }
+                    this.Dispatcher.Invoke(delegate ()
+                    {
+                        label1.Background = Brushes.Transparent;
+                    });
+                }
+                else
+                {
+                    /**Hűtés**/
+                    int i = label_ertek;    /// 20 fok
+                    while (i <= label_ertek + 4)            ///20 <= 24
+                    {
+                        Updater uiUpdater = new Updater(UpdateUI);
+                        Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, i);
+                        Thread.Sleep(300);
+                        i++;
+                    }
+
+                    this.Dispatcher.Invoke(delegate ()
+                    {
+                        label1.Background = Brushes.Blue;
+                    });
+                    for (int j = i; j >= allando; j--)          /// 24 --> 20 -->  
+                    {
+
+                        Updater uiUpdater = new Updater(UpdateUI);
+                        Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, j);
+                        Thread.Sleep(300);
+
+                    }
+                    this.Dispatcher.Invoke(delegate ()
+                    {
+                        label1.Background = Brushes.Transparent;
+                    });
+
+
+                }
+            }
+        }
+
+        private delegate void Updater(int UI);
+
+        private void UpdateUI(int i)
+        {
+            label1.Content = i;
+        }
+
+        private void kint_ho_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+        /***********************ÓRA***************************/
+        class SunData
+        {
+            private DateTime SunRise_Time;
+            private DateTime SunSet_Time;
+
+            public SunData(DateTime sunRise_Time, DateTime sunSet_Time)
+            {
+                SunRise_Time1 = sunRise_Time;
+                SunSet_Time1 = sunSet_Time;
+            }
+
+            public DateTime SunRise_Time1 { get => SunRise_Time; set => SunRise_Time = value; }
+            public DateTime SunSet_Time1 { get => SunSet_Time; set => SunSet_Time = value; }
+        }
+
+
+
+
+        private void DateClock()
+        {
+            var path = @"C:\Users\Felhasználó\Documents\GitHub\Szenzor\Bead\Client\Client\2020data.csv";
+            var lines = File.ReadAllLines(path);
+            List<SunData> sunDatas = new List<SunData>();
+            foreach (var line in lines)
+            {
+                string[] tokenek = line.Split(';');
+                int ev = int.Parse(tokenek[0]);
+                int honap = int.Parse(tokenek[1]);
+                int nap = int.Parse(tokenek[2]);
+                string[] napkelte_ido = tokenek[3].Split(':');
+                string[] napnyugta_ido = tokenek[4].Split(':');
+                DateTime napkelte = new DateTime(ev, honap, nap, int.Parse(napkelte_ido[0]), int.Parse(napkelte_ido[1]), 0);
+                DateTime napnyugta = new DateTime(ev, honap, nap, int.Parse(napnyugta_ido[0]), int.Parse(napnyugta_ido[1]), 0);
+                sunDatas.Add(new SunData(napkelte, napnyugta));
+            }
+
+            int[] napok = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            DateTime startDate = new DateTime(2020, 1, 1, 15, 0, 0);
+            this.Dispatcher.Invoke((Action)(() =>
+            {//this refer to form in WPF application 
+
+            }));
+
+            for (int i = 0; i < sunDatas.Count; i++)
+            {
+                for (int ho = 1; ho < 13; ho++)
+                {
+                    for (int nap = 1; nap <= napok[ho - 1]; nap++)
+                    {
+                        for (int ora = 0; ora < 25; ora++)
+                        {
+                            for (int perc = 0; perc < 60; perc++)
+                            {
+                                startDate = startDate.AddMinutes(1);
+                                Updater5 uiUpdater5 = new Updater5(DateUpdate);
+                                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater5, startDate.ToString("yyyy.MM.dd"));
+                                Updater3 uiUpdater3 = new Updater3(ClockUpdate);
+                                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater3, startDate.Hour);
+                                Updater4 uiUpdater4 = new Updater4(MinUpdate);
+                                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater4, startDate.Minute);
+
+                                Thread.Sleep(300);
+                                if (startDate == sunDatas[i].SunRise_Time1)
+                                {
+                                    this.Dispatcher.Invoke((Action)(() =>
+                                    {
+                                        this.LampLights.Text = "Lights off";
+                                    }));
+                                }
+                                else
+                                {
+                                    if (startDate == sunDatas[i].SunSet_Time1)
+                                    {
+                                        this.Dispatcher.Invoke((Action)(() =>
+                                        {
+                                            this.LampLights.Text = "Lights on";
+                                        }));
+                                    }
+                                }
+                            }
+                            //startDate.AddHours(Convert.ToDouble(ora));
+                        }
+                        //startDate.AddDays(Convert.ToDouble(nap));
+                    }
+                }
+            }
+        }
+
+        private void Clock()
+        {
+            int alap_ora1 = 0;
+
+            this.Dispatcher.Invoke((Action)(() =>
+            {//this refer to form in WPF application 
+
+                string alapora = this.ora.Content.ToString();
+                alap_ora1 = int.Parse(alapora);
+
+
+            }));
+            int i = alap_ora1;
+            while (true)
+            {
+
+                Updater3 uiUpdater3 = new Updater3(ClockUpdate);
+                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater3, i);
+                Thread.Sleep(2000);
+                i++;
+                if (i == 25)
+                {
+                    i = 0;
+                }
+
+                if (i >= 7 && i <= 17)
+                {
+                    isNappal = true;
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.LampLights.Text = "Lights off";
+                    }));
+                }
+                else
+                {
+                    isNappal = false;
+                }
+
+                if (i >= 18 && i <= 23)
+                {
+                    isEste = true;
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.LampLights.Text = "Lights on";
+                    }));
+
+                }
+                else
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.LampLights.Text = "Lights off";
+                    }));
+                    isEste = false;
+
+                }
+            }
+        }
+
+
+
+        private delegate void Updater3(int UI);
+
+        private void ClockUpdate(int i)
+        {
+            ora.Content = i;
+        }
+        private delegate void Updater4(int UI);
+
+        private void MinUpdate(int i)
+        {
+            if (i < 10)
+            {
+                perc.Content = "0" + i;
+            }
+            else
+            {
+                perc.Content = i;
+            }
+        }
+
+        private delegate void Updater5(string UI);
+
+        private void DateUpdate(string s)
+        {
+            datum.Text = s;
+        }
+
+
+        void TextBlock_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+
+            if (isNappal == false && isEste == false)
+            {
+                this.LampLights.Text = "Lights on";
+
+                Thread t = new Thread(new ThreadStart(LampaValtas));
+                t.Start();
+            }
+
+        }
+
+        void LampaValtas()
+        {
+            Thread.Sleep(1000);
+            Updater2 uiUpdater = new Updater2(UpdateUI2);
+            Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, "Lights off");
+
+
+
+        }
+
+        delegate void Updater2(string UI);
+
+        void UpdateUI2(string i)
+        {
+            this.LampLights.Text = i;
+
+        }
+
+
+
+
+        /***********************************************ÓRA*********************************************/
+
+
+
+
+
+        void TextBlock_IsMouseDirectlyOverChanged2(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+
+
+
+        }
+
+
+    }
+}
