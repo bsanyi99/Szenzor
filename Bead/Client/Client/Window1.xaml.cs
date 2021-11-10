b6d99afa76ac2783d967d684b739441dfeee1399
bsanyi99 <bs271999@mailbox.unideb.hu>
2021-11-10 09:24:52 +0100

    Date_simulation
    Szimuláció a 2020-as évhez, percekre pontva.

--- Bead/Client/Client/Window1.xaml.cs
+++ Bead/Client/Client/Window1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -119,7 +120,7 @@ namespace Client
                 Thread.Sleep(1000);
                 temp += 1;
             }
-            
+
         }
 
         private void Cool_Click(object sender, RoutedEventArgs e)
@@ -127,7 +128,7 @@ namespace Client
             Data dataToSend = new Data();
             dataToSend.cmdCommand = Command.Cool;
 
-            byte[] data = dataToSend.toByte(); 
+            byte[] data = dataToSend.toByte();
             clientSocket.Send(data);
 
         }
@@ -165,7 +166,7 @@ namespace Client
 
 
             ////// Ez az lámpa inditó
-            Thread t2 = new Thread(new ThreadStart(Clock));
+            Thread t2 = new Thread(new ThreadStart(DateClock));
             t2.Start();
 
 
@@ -270,6 +271,94 @@ namespace Client
 
 
         /***********************ÓRA***************************/
+        class SunData
+        {
+            private DateTime SunRise_Time;
+            private DateTime SunSet_Time;
+
+            public SunData(DateTime sunRise_Time, DateTime sunSet_Time)
+            {
+                SunRise_Time1 = sunRise_Time;
+                SunSet_Time1 = sunSet_Time;
+            }
+
+            public DateTime SunRise_Time1 { get => SunRise_Time; set => SunRise_Time = value; }
+            public DateTime SunSet_Time1 { get => SunSet_Time; set => SunSet_Time = value; }
+        }
+
+
+
+
+        private void DateClock()
+        {
+            var path = @"C:\Users\Felhasználó\Documents\GitHub\Szenzor\Bead\Client\Client\2020data.csv";
+            var lines = File.ReadAllLines(path);
+            List<SunData> sunDatas = new List<SunData>();
+            foreach (var line in lines)
+            {
+                string[] tokenek = line.Split(';');
+                int ev = int.Parse(tokenek[0]);
+                int honap = int.Parse(tokenek[1]);
+                int nap = int.Parse(tokenek[2]);
+                string[] napkelte_ido = tokenek[3].Split(':');
+                string[] napnyugta_ido = tokenek[4].Split(':');
+                DateTime napkelte = new DateTime(ev, honap, nap, int.Parse(napkelte_ido[0]), int.Parse(napkelte_ido[1]), 0);
+                DateTime napnyugta = new DateTime(ev, honap, nap, int.Parse(napnyugta_ido[0]), int.Parse(napnyugta_ido[1]), 0);
+                sunDatas.Add(new SunData(napkelte, napnyugta));
+            }
+
+            int[] napok = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            DateTime startDate = new DateTime(2020, 1, 1, 15, 0, 0);
+            this.Dispatcher.Invoke((Action)(() =>
+            {//this refer to form in WPF application 
+
+            }));
+
+            for (int i = 0; i < sunDatas.Count; i++)
+            {
+                for (int ho = 1; ho < 13; ho++)
+                {
+                    for (int nap = 1; nap <= napok[ho - 1]; nap++)
+                    {
+                        for (int ora = 0; ora < 25; ora++)
+                        {
+                            for (int perc = 0; perc < 60; perc++)
+                            {
+                                startDate = startDate.AddMinutes(1);
+                                Updater5 uiUpdater5 = new Updater5(DateUpdate);
+                                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater5, startDate.ToString("yyyy.MM.dd"));
+                                Updater3 uiUpdater3 = new Updater3(ClockUpdate);
+                                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater3, startDate.Hour);
+                                Updater4 uiUpdater4 = new Updater4(MinUpdate);
+                                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater4, startDate.Minute);
+
+                                Thread.Sleep(300);
+                                if (startDate == sunDatas[i].SunRise_Time1)
+                                {
+                                    this.Dispatcher.Invoke((Action)(() =>
+                                    {
+                                        this.LampLights.Text = "Lights off";
+                                    }));
+                                }
+                                else
+                                {
+                                    if (startDate == sunDatas[i].SunSet_Time1)
+                                    {
+                                        this.Dispatcher.Invoke((Action)(() =>
+                                        {
+                                            this.LampLights.Text = "Lights on";
+                                        }));
+                                    }
+                                }
+                            }
+                            //startDate.AddHours(Convert.ToDouble(ora));
+                        }
+                        //startDate.AddDays(Convert.ToDouble(nap));
+                    }
+                }
+            }
+        }
+
         private void Clock()
         {
             int alap_ora1 = 0;
@@ -277,7 +366,7 @@ namespace Client
             this.Dispatcher.Invoke((Action)(() =>
             {//this refer to form in WPF application 
 
-                string alapora = this.Ora.Content.ToString();
+                string alapora = this.ora.Content.ToString();
                 alap_ora1 = int.Parse(alapora);
 
 
@@ -286,7 +375,7 @@ namespace Client
             while (true)
             {
 
-                Updater3 uiUpdater3 = new Updater3(ClockUpdait);
+                Updater3 uiUpdater3 = new Updater3(ClockUpdate);
                 Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater3, i);
                 Thread.Sleep(2000);
                 i++;
@@ -333,11 +422,30 @@ namespace Client
 
         private delegate void Updater3(int UI);
 
-        private void ClockUpdait(int i)
+        private void ClockUpdate(int i)
         {
-            Ora.Content = i;
+            ora.Content = i;
+        }
+        private delegate void Updater4(int UI);
+
+        private void MinUpdate(int i)
+        {
+            if (i < 10)
+            {
+                perc.Content = "0" + i;
+            }
+            else
+            {
+                perc.Content = i;
+            }
         }
 
+        private delegate void Updater5(string UI);
+
+        private void DateUpdate(string s)
+        {
+            datum.Text = s;
+        }
 
 
         void TextBlock_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -385,7 +493,7 @@ namespace Client
         {
 
 
- 
+
 
         }
 
